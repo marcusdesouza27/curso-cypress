b3961408d6c3ab0a9c8d69a8b1b257d485c399e4
marcusdesouza27 <marcusdesouza27@gmail.com>
2020-11-22 13:32:59 -0300

    Iframes

--- cypress/integration/alerts.spec.js
+++ cypress/integration/alerts.spec.js
@@ -19,13 +19,85 @@ describe('Work with alerts', () => {
         })
     })
 
-    it.only('Alert test - com mock', () => {
+    it('Alert test - com mock', () => {
         const stub = cy.stub().as('Alerta')
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(() => {
-           var a = expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
+            var a = expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
 
 
+    })
+
+    it('Confirm - Aceitar', () => {
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it('Confirm - Negar', () => {
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Confirm Simples')
+            return false
+        })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it('Prompt - Aceitar', () => {
+
+        cy.window().then(win => {
+            cy.stub(win, 'prompt').as('prompt-mock')
+                .returns('42')
+        })
+
+        cy.on('window:confirm', msg => {
+            expect(msg).to.be.equal('Era 42?')
+            return false
+        })
+
+        // cy.on('window:prompt', msg => {
+        //     expect(msg).to.be.equal('Confirm Simples')
+        //     return false
+        // })
+        cy.on('window:alert', msg => {
+            expect(msg).to.be.equal(':(')
+        })
+        cy.get('#prompt').click()
+    })
+
+    it.only('Desafio - Alerts', () => {
+
+        const stub = cy.stub().as('alerta')
+        cy.on('window:alert', stub)
+
+        cy.get('#formCadastrar').click()
+            .then(() => 
+                expect(stub.getCall(0)).to.be.calledWith('Nome eh obrigatorio'))
+
+        cy.get('#formNome').type('Henrique')
+
+        cy.get('#formCadastrar').click()
+        .then(() => 
+            expect(stub.getCall(1)).to.be.calledWith('Sobrenome eh obrigatorio'))
+
+        cy.get('[data-cy=dataSobrenome]').type('Cruz')
+
+        cy.get('#formCadastrar').click()
+        .then(() => 
+            expect(stub.getCall(2)).to.be.calledWithNew('Sexo eh obrigatorio'))
+
+        cy.get('#formSexoMasc').click()
+        cy.get('#formCadastrar').click()
+
+        cy.get('#resultado > :nth-child(1)').should('contain.text', 'Cadastrado!')
     })
 })
